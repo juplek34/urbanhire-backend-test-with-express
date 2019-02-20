12635709adb82b01be7c64b040af93185e09d87f
Arief Yusron <ariefyusron98@gmail.com>
2019-02-21 03:34:08 +0700

    first fix

--- controllers/test.js
+++ controllers/test.js
@@ -1,22 +1,30 @@
-const first = async (req,res) => {
-  res.json('No 1')
-}
+const data = require('../data.json');
+
+const first = (req,res) => {
+  if(!req.query.search){
+    req.query.search = ''
+  }
+  const searchData = data.filter((item) => {
+    return item.toLowerCase().includes(req.query.search.toLowerCase())
+  })
+  res.json(searchData)
+};
 
 const second = async (req,res) => {
   res.json('No 2')
-}
+};
 
 const third = async (req,res) => {
   res.json('No 3')
-}
+};
 
 const fourth = async (req,res) => {
   res.json('No 4')
-}
+};
 
 const fifth = async (req,res) => {
   res.json('No 5')
-}
+};
 
 module.exports = {
   first,
@@ -24,4 +32,4 @@ module.exports = {
   third,
   fourth,
   fifth
-}
+};
