0cf4fe8995c50fc26832381464def0f6832ea473
Arief Yusron <ariefyusron98@gmail.com>
2019-02-21 05:12:02 +0700

    update in firstController

--- controllers/test.js
+++ controllers/test.js
@@ -1,17 +1,23 @@
 const data = require('../data.json');
 
 const first = (req,res) => {
-  if(!req.query.search){
-    req.query.search = ''
-  }
-  const searchData = data.filter((item) => {
-    return item.toLowerCase().includes(req.query.search.toLowerCase())
-  })
-
-  if(searchData.length===0){
-    res.json('empty')
+  if(Object.keys(req.query).length === 0){
+    res.json(data)
+  } else if(Object.keys(req.query).length > 1){
+    res.json('Params only 1 is search')
   } else {
-    res.json(searchData)
+    if(Object.keys(req.query)[0]!=='search'){
+      res.json('Not found')
+    } else {
+      const searchData = data.filter((item) => {
+        return item.toLowerCase().includes(req.query.search.toLowerCase())
+      })
+      if(searchData.length===0){
+        res.json('empty')
+      } else {
+        res.json(searchData)
+      }
+    }
   }
 };
 
